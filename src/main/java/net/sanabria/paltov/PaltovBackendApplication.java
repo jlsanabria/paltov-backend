ad18e2ca37b8b424b1a35fcf9ffd90ae6f843125
Jose Luis Sanabria <jlsanabria17@gmail.com>
2023-09-11 11:54:29 -0400

    Iniciando proyecto pALTOv

---
+++ src/main/java/net/sanabria/paltov/PaltovBackendApplication.java
@@ -0,0 +1,13 @@
+package net.sanabria.paltov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaltovBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaltovBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/sanabria/paltov/PaltovBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.sanabria.paltov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaltovBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
